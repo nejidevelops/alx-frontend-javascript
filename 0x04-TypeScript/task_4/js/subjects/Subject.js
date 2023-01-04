5c4747927e283f6eb379a623964ce193b2b426f2
Newton Ombese <newtonombese1@gmail.com>
2023-01-04 20:00:15 +0300

    task 4 Subject js file

---
+++ 0x04-TypeScript/task_4/js/subjects/Subject.js
@@ -0,0 +1,10 @@
+/// <reference path="Teacher.ts"/>
+var Subjects;
+(function (Subjects) {
+    var Subject = /** @class */ (function () {
+        function Subject() {
+        }
+        return Subject;
+    }());
+    Subjects.Subject = Subject;
+})(Subjects || (Subjects = {}));
