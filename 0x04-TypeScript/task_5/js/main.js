5b13f30f18eb74bd835465863a503f186587dfcb
Newton Ombese <newtonombese1@gmail.com>
2023-01-04 20:02:22 +0300

    task 5 main js file

---
+++ 0x04-TypeScript/task_5/js/main.js
@@ -0,0 +1,6 @@
+function sumMajorCredits(subject1, subject2) {
+    return (subject1.credits + subject2.credits);
+}
+function sumMinorCredits(subject1, subject2) {
+    return (subject1.credits + subject2.credits);
+}
